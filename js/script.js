9c7556dbddf4f137b8be46eafb81517ba81b0bd7
Stark <serj.elessar@gmail.com>
2021-09-03 15:35:26 +0300

    second commit adaptive NavBar

--- js/script.js
+++ js/script.js
@@ -1,9 +1,27 @@
+const header = document.querySelector('header');
+const burgerButton = document.querySelector('.burger-btn');
 const navBar = document.querySelector('nav');
+const navBarBurgerBtn = document.querySelector('.nav-adaptive__btn');
 
 window.addEventListener('scroll', () => {
   if (document.documentElement.scrollTop > 400) {
-    navBar.classList.add('sticky');
+    header.classList.add('sticky');
   } else {
-    navBar.classList.remove('sticky');
+    header.classList.remove('sticky');
   }
-})
+});
+
+burgerButton.addEventListener('click', () => {
+  burgerButton.classList.add('active');
+  navBar.classList.add('active');
+  navBarBurgerBtn.classList.add('active');
+  document.querySelector('body').classList.add('lock');
+  
+});
+
+navBarBurgerBtn.addEventListener('click', () => {
+  burgerButton.classList.remove('active');
+  navBar.classList.remove('active');
+  navBarBurgerBtn.classList.remove('active');
+  document.querySelector('body').classList.remove('lock');
+});
